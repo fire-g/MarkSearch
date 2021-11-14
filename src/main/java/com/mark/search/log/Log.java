2f44d52b1c97db59c7dd1127dfcae5c6849d2e0b
HaoTian <18296517259@163.com>
2021-11-15 01:28:16 +0800

    更新日志模型,添加了基础的日志分级

--- src/main/java/com/mark/search/log/Log.java
+++ src/main/java/com/mark/search/log/Log.java
@@ -13,10 +13,45 @@ import java.util.Locale;
  */
 public class Log {
 
+    @Deprecated
     public synchronized static void log(Class<?> clazz, Object args) {
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
         Date date = new Date();
         System.out.println(
                 df.format(date)+" " + clazz.getName() + "\t ::>> " + args.toString());
+    }
+
+    /**
+     * 日志记录级别Error,当程序运行发生错误时使用
+     * @param clazz 错误发生的类
+     * @param args 需要打印的数据
+     */
+    public synchronized static void Error(Class<?> clazz, Object args) {
+        log(clazz,"Error",args);
+    }
+
+    /**
+     * 日志记录级别Debug,当程序运行调试时使用
+     * @param clazz 错误发生的类
+     * @param args 需要打印的数据
+     */
+    public synchronized static void Debug(Class<?> clazz, Object args) {
+        log(clazz,"DEBUG",args);
+    }
+
+    /**
+     * 日志记录级别Info,当程序运行发生时记录信息使用
+     * @param clazz 错误发生的类
+     * @param args 需要打印的数据
+     */
+    public synchronized static void Info(Class<?> clazz, Object args) {
+        log(clazz,"INFO",args);
+    }
+
+    private synchronized static void log(Class<?> clazz,String type,Object args){
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+        Date date = new Date();
+        System.out.println(
+                type+" "+ " "+  df.format(date)+" " + clazz.getName()  + args.toString());
     }
 }
